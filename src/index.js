27891654256458ef43913169da74c4b0b0554a5a
Rezaul Karim <82644789+rezaulkarim7080@users.noreply.github.com>
2024-01-02 22:06:16 +0600

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserAuthContextProvider } from './context/UserAuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <UserAuthContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </UserAuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
